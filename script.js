340be96a4daa6262022c45bcd741b46e788fcb1f
Lucas <40408423+Pulucas@users.noreply.github.com>
2025-06-25 21:53:47 +0200

    - sounds -  and other stuff...

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const screen = {
+  square: {
+    1: { element: document.getElementById("1") },
+    2: { element: document.getElementById("2") },
+    3: { element: document.getElementById("3") },
+    4: { element: document.getElementById("4") },
+  },
   start: {
     element: {
       box: document.getElementById("start-screen-box"),
@@ -36,41 +42,80 @@ const screen = {
       text: document.getElementById("logo-text"),
     },
     update: (roundNum) => {
-      const text = screen.logo.element.text.innerHTML.split("<br>");
-      text[text.length - 1] = roundNum.toString();
-      screen.logo.element.text.innerHTML = text.join("<br>");
+      const element = screen.logo.element.text;
+      const newTextArr = element.innerText.split(" ");
+      newTextArr[1] = roundNum.toString(); // update round number
+      screen.logo.element.text.innerText = newTextArr.join(" ");
     }
-  }
-}
-const endScreenMessage = document.getElementById("end-screen-message")
-const square = {
-  1: { element: document.getElementById("1") },
-  2: { element: document.getElementById("2") },
-  3: { element: document.getElementById("3") },
-  4: { element: document.getElementById("4") },
+  },
+  audio: {
+    context: new AudioContext,
+    frequencies: {
+      1: 165,
+      2: 220,
+      3: 277,
+      4: 330,
+    },
+    activeOscillators: {
+      1: undefined,
+      2: undefined,
+      3: undefined,
+      4: undefined,
+    },
+    startSound: (color) => {
+      const context = screen.audio.context;
+
+      // make oscillator
+      const oscillator = context.createOscillator();
+
+      // configure oscillator
+      oscillator.type = "triangle";
+      oscillator.frequency.value = screen.audio.frequencies[color];
+      oscillator.connect(context.destination);
+
+      // play sound
+      oscillator.start();
+
+      // save current oscillator
+      screen.audio.activeOscillators[color] = oscillator;
+    },
+    stopSound: (color) => {
+      const oscillator = screen.audio.activeOscillators[color];
+      oscillator.stop();
+    },
+  },
 }
 
 const gameState = {
+  lengthBetweenHighlights: 300,
   sequence: [],
   buttonPressCount: 0,
   winLength: 20,
   isMobile: !!('ontouchstart' in window || navigator.maxTouchPoints > 0), // checks if on mobile, turns result into boolean value
 }
 
+if (gameState.isMobile) {
+  const logo = document.getElementsByClassName("logo");
+  const logoUnder = document.getElementsByClassName("logo-under");
+
+  logo[0].classList.add("mobile");
+  logoUnder[0].classList.add("mobile");
+}
+
 function startGame() {
   // present start screen
   screen.start.open();
-
+  
   // wait until start button is pressed
-  function waitForButtonPress(event) {
+  async function waitForButtonPress(event) {
     console.log("Starting Game");
-
+    
     // hide start screen
     screen.start.close();
-
+    
     // play the game
     main();
-
+    
     // remove event listener
     console.log("removed event listener on start screen button");
     screen.start.element.button.removeEventListener('click', waitForButtonPress);
@@ -82,10 +127,10 @@ function startGame() {
 async function main() {
   // add 1 rng to sequence
   gameState.sequence.push(RNG());
-
+  
   // update level number
   screen.logo.update(gameState.sequence.length);
-
+  
   // highlight the squares
   await highlightSquares(gameState.sequence);
 
@@ -188,35 +233,35 @@ async function toggleRecordUserInputs(toggleOn, resolve) {
   const eventType = gameState.isMobile ? "touchend" : "mouseup";
   for (let i = 1; i <= 4; i++) { // goes through all squares
     if (toggleOn) {
-      square[i].recordHandler = function(event) {
+      screen.square[i].recordHandler = function(event) {
         addListenerRecordUserInput(event, resolve);
       }
-      square[i].element.addEventListener(eventType, square[i].recordHandler);
+      screen.square[i].element.addEventListener(eventType, screen.square[i].recordHandler);
       console.log("added click listeners");
     } else {
       
-      square[i].element.removeEventListener(eventType, square[i].recordHandler);
-      square[i].recordHandler = null;
+      screen.square[i].element.removeEventListener(eventType, screen.square[i].recordHandler);
+      screen.square[i].recordHandler = null;
       console.log("removed click listeners");
     }
   }
 }
 
-function highlightSquare(event) { event.target.classList.add("highlight"); };
-function unhighlightSquare(event) { event.target.classList.remove("highlight"); };
+function highlightSquare(event) { event.target.classList.add("highlight"); screen.audio.startSound(event.target.id); };
+function unhighlightSquare(event) { event.target.classList.remove("highlight"); screen.audio.stopSound(event.target.id); };
 function toggleTapping(toggleOn) {
   const eventTypeClick = gameState.isMobile ? "touchstart" : "mousedown";
   const eventTypeUnclick = gameState.isMobile ? "touchend" : "mouseup";
   for (let i = 1; i <= 4; i++) { // goes through all squares
     if (toggleOn) { // if there's no event listener for a given square
       // adds a highlight to the square when clicked
-      square[i].element.addEventListener(eventTypeClick, highlightSquare);
-      square[i].element.addEventListener(eventTypeUnclick, unhighlightSquare);
+      screen.square[i].element.addEventListener(eventTypeClick, highlightSquare);
+      screen.square[i].element.addEventListener(eventTypeUnclick, unhighlightSquare);
       console.log("added highligh listeners");
     } else {
       // removes event listener
-      square[i].element.removeEventListener(eventTypeClick, highlightSquare);
-      square[i].element.removeEventListener(eventTypeUnclick, unhighlightSquare);
+      screen.square[i].element.removeEventListener(eventTypeClick, highlightSquare);
+      screen.square[i].element.removeEventListener(eventTypeUnclick, unhighlightSquare);
       console.log("removed highligh listeners");
     }
   }
@@ -224,10 +269,12 @@ function toggleTapping(toggleOn) {
 
 async function highlightSquares(squaresArr) {
   for (const i of squaresArr) {
-    await new Promise((res) => { setTimeout(res, 550) });
-    square[i].element.classList.add("highlight");
-    await new Promise((res) => { setTimeout(res, 300) });
-    square[i].element.classList.remove("highlight");
+    await new Promise((res) => { setTimeout(res, gameState.lengthBetweenHighlights + 100) });
+    screen.square[i].element.classList.add("highlight");
+    screen.audio.startSound(i);
+    await new Promise((res) => { setTimeout(res, gameState.lengthBetweenHighlights) });
+    screen.square[i].element.classList.remove("highlight");
+    screen.audio.stopSound(i);
   }
   return;
 }
@@ -236,4 +283,4 @@ function RNG() {
   return Math.ceil(Math.random() * 4);
 }
 
-startGame();
+startGame(); // run the main game
